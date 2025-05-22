e1e37be5146c205a915017c3b63f9278f3fb0849
refrigerator2 <ti.mak4710@gmail.com>
2025-05-22 17:57:14 +0200

    models

--- project/backend/index.js
+++ project/backend/index.js
@@ -51,4 +51,45 @@ app.post('/login', async (req, res) => {
     }
 })
 
+app.post('/newthread', async (req, res) => {
+    const { title, user } = req.body;
+
+    if (!title || !user) {
+        return res.status(400).json({ error: 'Не указаны обязательные поля: title или user' });
+    }
+
+    try {
+        const thread = await Thread.create({
+            title: title,
+            created_by: user,
+            created_at: new Date(),
+        });
+
+        res.status(201).json({ message: 'Тема создана', thread });
+    } catch (err) {
+        console.error('Ошибка при создании темы:', err);
+        res.status(500).json({ error: 'Ошибка сервера' });
+    }
+});
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({error: "Username or Password not entered"});
+    }
+    const user = await User.findOne({where: username});
+    if(user){
+        return res.status(401).json({error: "User is already existing"})
+    }
+    try{
+        const newuser = await User.create({
+            username: username,
+            password: password,
+            createdAt: new Date(),
+        })
+        res.status(200).json({message: "User created successfully"})
+    }
+    catch(err){
+        console.log(err);
+    }
+})
 start();
