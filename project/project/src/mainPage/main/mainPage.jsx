e6f7c63d263ce50d9c73153372a594eb0bd96795
1nrez <formyyworkk@gmail.com>
2025-06-12 20:42:06 +0200

    adaptation on mobile

--- project/project/src/mainPage/main/mainPage.jsx
+++ project/project/src/mainPage/main/mainPage.jsx
@@ -50,7 +50,7 @@ const MainPage = () => {
         setThreadsFilter(filter);
         setIsMobileSidebarOpen(false); // закрываем меню после выбора
     };
-    ////////
+    //////////////
 
     return (
         <>
